5e6cb079cfc10fc632ccd039cc458537bc2773be
pradeeshjeeva0603-png <pradeeshjeeva0603@gmail.com>
2026-09-20 19:47:56 +0530

    Secure database credentials and update Git ignore

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+load_dotenv()
 def get_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Pradeesh@0076",
+        password=os.getenv("DB_PASSWORD"),
         database="quantumedge_corporation"
     )
     return connection
